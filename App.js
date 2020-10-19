05cbaaa9a19e96f0b509a88c6468e0d08e822d8a
Raphael Albern <ralpha19@hotmail.com>
2020-10-19 15:42:44 -0400

    project set up

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react"
+import Header from "./Header"
+import MemeGenerator from "./MemeGenerator"
+
+function() {
+  return {
+    <Header />
+    <MemeGenerator />
+  }
+}
